0cb9a16b063514e2194c16debed9ee5120d4959f
abyCherian <akashicrecordx@gmail.com>
2025-03-16 11:51:39 +0530

    Web project

---
+++ script.js
@@ -0,0 +1,26 @@
+const btnEl = document.getElementById("btn")
+let jokeEL = document.getElementById("joke")
+
+const apiKey = "jSOkhJ8UG5ifqIuR0r5dGw==L7oeiKwm40WHlk0O"
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key" : apiKey
+    }
+}
+
+let apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1'
+
+async function getJoke(){
+    
+    jokeEL.innerText = "Updating.."
+    let response = await fetch(apiURL, options);
+    let data = await response.json();
+    
+    jokeEL.innerText = data[0].joke;
+
+
+}
+
+btnEl.addEventListener("click", getJoke)
